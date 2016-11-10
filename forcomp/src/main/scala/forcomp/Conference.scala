2db2e283fcb3c77ce5cecb454df8ab93d5293f58
Aco Mitevski <aco.mitevski@gmail.com>
2016-11-10 18:12:55 +0100

    for comprehensions course

--- forcomp/src/main/scala/forcomp/Conference.scala
+++ forcomp/src/main/scala/forcomp/Conference.scala
@@ -37,31 +37,42 @@ case class Talk(title: String, themes: Set[String], time: Time)
 case class Track(talks: Seq[Talk]) {
 
   /** all talks in this track on the theme */
-  def talksOn(theme: String): Seq[Talk] = ??? // TODO implement using .filter
+  def talksOn(theme: String): Seq[Talk] = talks.filter(_.themes.contains(theme))
 
   /** the talk in this track at the given time */
-  def talkAt(time: Time): Option[Talk] = ??? // TODO implement using .find
+  def talkAt(time: Time): Option[Talk] = talks.find(_.time == time)
 
   /** all themes of talks in this track */
-  def themes: Set[String] = ??? // TODO implement using .flatMap and .to[Set]
+  def themes: Set[String] = talks.to[Set].flatMap(_.themes)
 
 }
 
 case class Schedule(tracks: Set[Track]) {
 
   // all talks
-  def talks: Set[Talk] = ??? // TODO implement using .flatMap
+  def talks: Set[Talk] = tracks.flatMap(_.talks)
 
   // all talks at the given time on the theme
-  def talksOn(time: Time, theme: String): Set[Talk] = ??? // TODO implement using for-comprehension
+  def talksOn(time: Time, theme: String): Set[Talk] = for {
+    track <- tracks
+    talk <- track.talkAt(time)
+    if talk.themes.contains(theme)
+  } yield talk
 
   // counts how often a theme occurs overall
   // Optional task; write tests along with the implementation.
-  // def themeCount: Map[String, Int] = ???
+   def themeCount: Map[String, Int] = {
+    for {
+      (theme,list) <- talks.to[Seq].flatMap(_.themes).groupBy(identity)
+    } yield (theme, list.size)
+//    allThemes.groupBy(identity).map(p => (p._1, p._2.size))
+  }
 
   // ranking of themes in descending order of occurrence in this schedule.
   // Optional task; write tests along with the implementation.
-  // def themeRank: List[String] = ???
+   def themeRank: List[String] = {
+    themeCount.toList.sortBy(_._2).reverse.map(_._1)
+  }
 }
 
 /**
